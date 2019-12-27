87e88b138660abce3dd9a251672bc78edc55d789
Maxim Sozinov <max.sozinov@gmail.com>
2019-12-27 19:32:39 +0700

    decorate board done

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -1,8 +1,8 @@
-const rowsCount = 8;
-const colsCount = 8;
 
-const cols = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '0'];
+const cols = ['0', '8', '7', '6', '5', '4', '3', '2', '1', '0'];
 const rows = ['0', 'a', 'b', 'c', 'd', 'f', 'e', 'g', 'h', '0'];
+
+const playerSet = 'white';
 
 
 let renderer = {
@@ -13,31 +13,18 @@ let renderer = {
     renderBoard() {
         let result = this.generateBoard();
         document.body.insertAdjacentHTML("afterbegin", result);
-        // this.renderUserPosition(player);
+        this.decorateBoard();
     },
 
     /**
      * Метод генерирует игровое поле на основании размеров в константах.
      * @returns {string} сгенерированный html-код таблицы(игрового поля).
      */
-
-    // generateBoard() {
-    //     let board = "";
-    //     for (let y = 0; y < rowsCount; y++) {
-    //         board += "<tr>";
-    //         for (let x = 0; x < colsCount; x++) {
-    //             board += `<td data-x="${x}" data-y="${y}"></td>`;
-    //         }
-    //         board += "</tr>";
-    //     }
-    //     return `<table><tbody>${board}</tbody></table>`;
-    // },
-
     generateBoard() {
         let board = "";
         let cellColor = '';
         let cellNumber = 0;
-        
+
         cols.forEach(col => {
             board += "<tr>";
             rows.forEach(row => {
@@ -58,8 +45,21 @@ let renderer = {
         return `<table><tbody>${board}</tbody></table>`;
     },
 
-    decorateBoard() {
 
+    /**
+     * Метод подписывает строки и столбцы игрового поля
+     *
+     */
+    decorateBoard() {
+        const edgeCells = document.querySelectorAll('.edgeCell');
+        edgeCells.forEach(cell => {
+            if (cell.dataset.y === "0" && cell.dataset.x !== "0") {
+                cell.innerText =  cell.dataset.x;
+            }
+            if (cell.dataset.x === "0" && cell.dataset.y !== "0") {
+                cell.innerText =  cell.dataset.y;
+            }
+        });
     }
 
 };
